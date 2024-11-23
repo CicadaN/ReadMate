a0e227a9948c674e852fba24d73d96a6c009123e
cicad <cicadac@ya.ru>
2024-11-23 20:24:01 +0300

    Init commit

---
+++ build.gradle.kts
@@ -0,0 +1,55 @@
+plugins {
+	java
+	id("org.springframework.boot") version "3.3.6"
+	id("io.spring.dependency-management") version "1.1.6"
+	id("gg.jte.gradle") version "3.1.12"
+}
+
+group = "com.example"
+version = "0.0.1-SNAPSHOT"
+
+java {
+	toolchain {
+		languageVersion = JavaLanguageVersion.of(21)
+	}
+}
+
+configurations {
+	compileOnly {
+		extendsFrom(configurations.annotationProcessor.get())
+	}
+}
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-data-jdbc")
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+	implementation("org.springframework.boot:spring-boot-starter-data-rest")
+	implementation("org.springframework.boot:spring-boot-starter-jdbc")
+	implementation("org.springframework.boot:spring-boot-starter-oauth2-authorization-server")
+	implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
+	implementation("org.springframework.boot:spring-boot-starter-security")
+	implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("org.springframework.boot:spring-boot-starter-web-services")
+	implementation("gg.jte:jte:3.1.12")
+	implementation("gg.jte:jte-spring-boot-starter-3:3.1.12")
+	implementation("org.thymeleaf.extras:thymeleaf-extras-springsecurity6")
+	compileOnly("org.projectlombok:lombok")
+	annotationProcessor("org.projectlombok:lombok")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation("org.springframework.security:spring-security-test")
+	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+jte {
+	generate()
+	binaryStaticContent = true
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
---
+++ src/test/java/com/example/ReadMate/ReadMateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ReadMate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReadMateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
